d569cddb7d22dfc59542ca673e9e7fd16fc0e56d
Taylor <taylorcgaines@gmail.com>
2022-04-03 10:45:16 -0600

    checkpoint, styling and html done

--- components/blocks/NavHeader.vue
+++ components/blocks/NavHeader.vue
@@ -1,67 +1,128 @@
 <template>
-  <div ref="nav-container" class="nav-container">
-    <div ref="nav-overlay" class="nav-overlay" :style="navOverlay"></div>
-    <nav ref="nav-fullscreen" class="nav-fullscreen">
-      <ul ref="list">
-        <li><a href="#" title="">Github</a></li>
-        <li><a href="https://discord.leaderboards.gg" title="">Discord</a></li>
-        <li><a href="#" title="">Info and FAQs</a></li>
-        <li><a href="#" title="">Blog</a></li>
-        <li><a href="#" title="">Contact</a></li>
-      </ul>
-    </nav>
-    <a ref="nav-toggle" class="nav-toggle" @click="toggleNav">
-      <span></span>
-      <span></span>
-      <span></span>
-    </a>
+  <div ref="nav-header" class="nav-header">
+    <div class="nav-container">
+      <div ref="nav-overlay" class="nav-overlay open"></div>
+      <nav ref="nav-fullscreen" class="nav-fullscreen open">
+        <ul>
+          <li><a href="https://discord.leaderboards.gg">Discord</a></li>
+          <li><a href="https://github.com/leaderboardsgg">Github</a></li>
+        </ul>
+      </nav>
+      <a ref="nav-toggle" class="nav-toggle open" href=""><span></span><span></span><span></span></a>
+    </div>
   </div>
 </template>
 
 <script>
 export default{
   name: 'NavHeader',
-  data() {
-    return {
-      navOpen: false,
-      navFullscreen: {
-        // width: 0,
-        // height: 0,
-      },
-      navOverlay: {
-        // width: 0,
-        // height: 0,
-        // marginTop: 0,
-        // marginLeft: 0,
-      },
-    };
-  },
-  methods: {
-    toggleNav() {
-      if (!this.navOpen) {
-        this.$refs["nav-toggle"].classList.add('open');
-        this.$refs["nav-overlay"].classList.add('open');
-        this.$refs["nav-fullscreen"].classList.add('open');
-        this.navOpen = true;
-      } else {
-        this.$refs["nav-toggle"].classList.remove('open');
-        this.$refs["nav-overlay"].classList.remove('open');
-        this.$refs["nav-fullscreen"].classList.remove('open');
-        this.navOpen = false;
-      }
-
-    },
-
-    handleResize() {
-      this.$refs["nav-fullscreen"].height = window.innerHeight;
-
-      const radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
-      const diameter = radius * 2;
-      this.$refs["nav-overlay"].width = diameter;
-      this.$refs["nav-overlay"].height = diameter;
-      this.navOverlay.marginTop = radius;
-      this.navOverlay.marginLeft = radius;
-    }
-  },
 }
 </script>
+
+<style scoped lang="scss">
+.nav-header {
+  outline: 1px solid palevioletred;
+  display: flex;
+  padding: 16px;
+  overflow: hidden;
+  color: white;
+
+  .nav-container {
+    margin-left: auto;
+    position: relative;
+
+    .nav-toggle {
+      display: block;
+      cursor: pointer;
+      transform: rotate(0deg);
+
+      span {
+        background-color: black;
+        width: 100%;
+        display: block;
+        position: absolute;
+        left: 0;
+        content: '';
+        transform-origin: left center;
+        // transition time and time function
+        transition: .5s ease-in-out;
+      }
+
+      &.open {
+        span {
+          &:nth-child(1) {
+            transform: rotate(45deg);
+          }
+
+          &:nth-child(2) {
+            width: 0;
+            opacity: 0;
+          }
+
+          &:nth-child(3) {
+            transform: rotate(-45deg);
+          }
+        }
+      }
+    }
+    .nav-overlay {
+      position: absolute;
+      z-index: -1;
+      background: rgba(0, 0, 0, 0.8);
+      border-radius: 50%;
+      transition: 1s;
+      transform: scale3d(0, 0, 0);
+      &.open {
+        transform: scale3d(1, 1, 1);
+      }
+    }
+    .nav-fullscreen {
+      width: 100%;
+      position: fixed;
+      left: 0;
+      top: 0;
+
+      transition: ease-in-out 0.25s;
+      transition-delay: 0s;
+
+      visibility: hidden;
+      opacity: 0;
+
+      ul {
+        list-style: none;
+      }
+
+      a {
+        color: white;
+        text-decoration: none;
+      }
+      &.open{
+        visibility: visible;
+        opacity: 1;
+        transition: ease-in-out 0.5s;
+        transition-delay: 0.25s;
+      }
+    }
+    .nav-overlay{
+      background: linear-gradient(180deg, #FF512F 10%, #FF512F 50%, #DD2476 90%);
+    }
+    .nav-toggle span {
+      background-color: white;
+    }
+    .nav-fullscreen {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      color: white;
+      font-family: sans-serif;
+      font-size: 10vh;
+
+      li:hover {
+        list-style: disc;
+      }
+    }
+  }
+}
+
+</style>
