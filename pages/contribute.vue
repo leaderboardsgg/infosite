95d068327b423a3d727ae99d42b8ef16f9033dda
squiddot <taylorcgaines@gmail.com>
2023-09-12 13:52:15 -0700

    updated content, blur background

--- pages/contribute.vue
+++ pages/contribute.vue
@@ -1,60 +1,87 @@
 <template>
   <div class="page">
-    <HeroBanner color="blue"><h1><span>How You Can Help</span></h1></HeroBanner>
+    <HeroBanner color="blue"
+      ><h1><span>How You Can Help</span></h1></HeroBanner
+    >
     <h3 class="intro">Contributor Guidelines</h3>
-    <div>
-      <h2>Thank you for your interest in contributing to LeaderboardsGG. We're glad you found your way here, and we're always in need of new contributors.</h2>
+    <div class="container">
+      <h2>
+        Thank you for your interest in contributing to LeaderboardsGG. We're
+        glad you found your way here, and we're always in need of new
+        contributors.
+      </h2>
 
       <h2>General Information</h2>
-      <p>We currently have three repositories set up. Below are the links to each one along with some basic information regarding their respective technology stacks. Make sure to read the contributing guidelines for the repository you wish to contribute to. This will ensure a smooth experience for all involved.</p>
+      <p>
+        We currently have three repositories set up. Below are the links to each
+        one along with some basic information regarding their respective
+        technology stacks. Make sure to read the contributing guidelines for the
+        repository you wish to contribute to. This will ensure a smooth
+        experience for all involved.
+      </p>
 
       <h2>Our Repositories</h2>
 
       <h3>Front End</h3>
       <ul>
+        <li>Technologies: Vue.js, Nuxt.js, Jest, TailwindCSS</li>
         <li>
-          Technologies: Vue.js, Nuxt.js, Jest, TailwindCSS
+          <a href="https://github.com/leaderboardsgg/leaderboard-site"
+            >Github Repo</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-site">Github Repo</a>
+          <a
+            href="https://github.com/leaderboardsgg/leaderboard-site/blob/main/CONTRIBUTING.md"
+            >Contributing Guidelines</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-site/blob/main/CONTRIBUTING.md">Contributing Guidelines</a>
-        </li>
-        <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-site/issues">Issues</a>
+          <a href="https://github.com/leaderboardsgg/leaderboard-site/issues"
+            >Issues</a
+          >
         </li>
       </ul>
 
       <h3>Back End</h3>
       <ul>
+        <li>Technologies: C#, ASP.NET Core, .NET 6.0, PostgreSQL</li>
         <li>
-          Technologies: C#, ASP.NET Core, .NET 6.0, PostgreSQL
+          <a href="https://github.com/leaderboardsgg/leaderboard-backend"
+            >Github Repo</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-backend">Github Repo</a>
+          <a
+            href="https://github.com/leaderboardsgg/leaderboard-backend/blob/main/CONTRIBUTING.md"
+            >Contributing Guidelines</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-backend/blob/main/CONTRIBUTING.md">Contributing Guidelines</a>
-        </li>
-        <li>
-          <a href="https://github.com/leaderboardsgg/leaderboard-backend">Issues</a>
+          <a href="https://github.com/leaderboardsgg/leaderboard-backend"
+            >Issues</a
+          >
         </li>
       </ul>
 
       <h3>Info Site</h3>
       <ul>
+        <li>Technologies: Vue.js, Nuxt.js, SCSS</li>
         <li>
-          Technologies: Vue.js, Nuxt.js, SCSS
+          <a href="https://github.com/leaderboardsgg/poc-infosite"
+            >Github Repo</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/poc-infosite">Github Repo</a>
+          <a
+            href="https://github.com/leaderboardsgg/poc-infosite/blob/main/CONTRIBUTING.md"
+            >Contributing Guidelines</a
+          >
         </li>
         <li>
-          <a href="https://github.com/leaderboardsgg/poc-infosite/blob/main/CONTRIBUTING.md">Contributing Guidelines</a>
-        </li>
-        <li>
-          <a href="https://github.com/leaderboardsgg/poc-infosite/issues">Issues</a>
+          <a href="https://github.com/leaderboardsgg/poc-infosite/issues"
+            >Issues</a
+          >
         </li>
       </ul>
 
@@ -65,15 +92,20 @@
 </template>
 
 <script>
-import HeroBanner from "@/components/blocks/HeroBanner";
+import HeroBanner from '@/components/blocks/HeroBanner'
 export default {
   components: { HeroBanner },
   layout: 'Synthwave',
 }
 </script>
 
-<style scoped>
-h2{
+<style scoped lang="scss">
+
+.page {
+  backdrop-filter: blur(10px);
+}
+h2,
+h3 {
   margin-bottom: 25px;
   margin-top: 25px;
 }
